5a77396024937ead1a4b676de1a07fe107d63f7a
Sam Miller <sm1215@gmail.com>
2019-02-19 16:14:43 -0500

    routing changes

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,16 +1,67 @@
 import Route from '@ember/routing/route';
-import EmberObject from '@ember/object';
 import { A } from '@ember/array';
+import EmberObject, { computed } from '@ember/object';
+import { dasherize } from '@ember/string';
 
 let Band = EmberObject.extend({
   name: '',
+  language: '',
+  slug: computed('name', function() {
+    console.log("Recomputing slug");
+    return dasherize(this.name);
+  }),
+  site: computed('slug', 'language', function() {
+    console.log("Recomputing site");
+    return `https://bands.com/${this.slug}.${this.language}`;
+  })
+});
+
+let Song = EmberObject.extend({
+  title: '',
+  rating: 0,
+  band: ''
 });
 
 export default Route.extend({
   model() {
-    let ledZeppelin = Band.create({ name: 'Led Zeppelin' });
-    let pearlJam = Band.create({ name: 'Pearl Jam' });
-    let fooFighters = Band.create({ name: 'Foo Fighters' });
+    let blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zepplin',
+      rating: 3
+    });
+
+    let yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    let pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    let daughter = Song.create({
+      title: 'Daughter',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    let ledZeppelin = Band.create({ 
+      name: 'Led Zeppelin', 
+      songs: A([blackDog]) 
+    });
+
+    let pearlJam = Band.create({ 
+      name: 'Pearl Jam', 
+      songs: A([yellowLedbetter, daughter]) 
+    });
+
+    let fooFighters = Band.create({ 
+      name: 'Foo Fighters', 
+      songs: A([pretender]) 
+    });
 
     return A([ledZeppelin, pearlJam, fooFighters]);
   }
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -10,24 +10,6 @@ let Song = EmberObject.extend({
 
 export default Route.extend({
   model() {
-    let blackDog = Song.create({
-      title: 'Black Dog',
-      band: 'Led Zepplin',
-      rating: 3
-    });
-
-    let yellowLedbetter = Song.create({
-      title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
-      rating: 4
-    });
-
-    let pretender = Song.create({
-      title: 'The Pretender',
-      band: 'Foo Fighters',
-      rating: 2
-    });
-
-    return A([blackDog, yellowLedbetter, pretender]);
+    return this.modelFor('bands.band');
   }
 });

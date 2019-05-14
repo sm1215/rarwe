eecc481ed638d3183afd00a34647b99c5f9256ba
Sam Miller <sm1215@gmail.com>
2019-05-14 15:29:39 -0400

    queryParams work

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,9 @@
 export default function() {
   this.get('bands');
-  this.get('bands/:id');
   this.post('bands');
+  this.get('bands/:id');
+  this.get('bands/:id/songs', function(schema, request) {
+    let id = request.params.id;
+    return schema.songs.where({ bandId: id });
+  });
 }
